dd6feb28ddf7eb431d80a5f1880be7b66afd0938
Denis Sokolov <denis.sokolow@gmail.com>
2022-09-11 00:32:53 +0300

    two

---
+++ src/components/Content.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Content() {
+  return (
+    <div className='content'>
+       <div className='leftblock'></div>
+       
+    </div>
+  )
+}
+
+export default Content
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='nav'>
+        <p>Обо мне</p>
+        <p>Проекты</p>
+        <p>Контакты</p>
+    </div>
+  )
+}
+
+export default Navbar
