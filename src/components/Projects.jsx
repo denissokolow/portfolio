e8cf55de12c1211f11b5ad1345ebe5dc2dbef1b0
denissokolow <denis.sokolow@gmail.com>
2022-09-15 20:07:28 +0300

    five

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import mern from '../img/mern.jpg'
+import reactjs from '../img/reactjs.jpg'
+import landing from '../img/landing.jpg'
 
 function Projects() {
   return (
-    <div>Projects</div>
+    <div className='projects'>
+      <h1>Мои проекты</h1>
+      <div className='card'><img src={mern} alt="MERN"></img></div>
+      <div className='card'><img src={reactjs} alt="REACTJS"></img></div>
+      <div className='card'><img src={landing} alt="LANDING"></img></div>
+    </div>
   )
 }
 
