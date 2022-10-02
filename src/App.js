749063ffba7845a5f73492ceca7b53ea0233f7ae
Denis Sokolov <denis.sokolow@gmail.com>
2022-10-02 16:08:24 +0300

    fin

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import DotLoader from "react-spinners/DotLoader"
 import Navbar from "./components/Navbar";
 import About from "./components/About";
 import Contacts from "./components/Contacts"
@@ -6,7 +8,22 @@ import Projects from "./components/Projects"
 import "./App.css"
 
 function App() {
-  return (
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3500);
+  }, []);
+
+  return isLoading ?
+    <DotLoader
+      display={'block'}
+      margin-top={'10%'}
+      color={'rgb(88, 86, 86  )'}
+      size={'30'}
+      isLoading={isLoading}
+    /> :
     <div className="container">
       <Navbar />
       <Routes>
@@ -16,8 +33,6 @@ function App() {
         <Route path='*' element={<About />} />
       </Routes>
     </div>
-
-  );
 }
 
 export default App;
